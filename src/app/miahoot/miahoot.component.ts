3796765f87c1b21e821e6313328c726d15cd4179
rayanne <rayanne74@hotmail.fr>
2023-05-03 10:23:36 +0200

    reponse v0

--- src/app/miahoot/miahoot.component.ts
+++ src/app/miahoot/miahoot.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import {QCMProjected, Question, MiahootService, ProjectedMiahoot} from "./miahoot.service";
 import {MiahootModule} from "./miahoot.module";
 import {CommonModule} from "@angular/common";
 import {BehaviorSubject, combineLatest, map, Observable} from "rxjs";
 import {set} from "@angular/fire/database";
+import { FormsModule } from '@angular/forms';
 
 @Component({
 
@@ -11,11 +12,12 @@ import {set} from "@angular/fire/database";
     templateUrl: './miahoot.component.html',
     styleUrls: ['./miahoot.component.scss'],
     standalone : true,
-    imports : [MiahootModule,CommonModule],
+    imports : [MiahootModule,CommonModule, FormsModule],
 })
 export class MiahootComponent {
   questionObs: Observable<undefined|QCMProjected>;
   miahootObs: Observable<undefined|ProjectedMiahoot>;
+  responses: boolean[] = [];
 
   constructor(private miahootService:MiahootService) {
     this.miahootObs = miahootService.obsProjectedMiahoot;
@@ -26,6 +28,13 @@ export class MiahootComponent {
     this.miahootService.setNextQuestion();
   }
 
-    protected readonly set = set;
+  protected readonly set = set;
+
+  responseChange(event: any, index: number) {
+    const checked = event.target.checked;
+    this.responses[index] = checked;
+    console.log(this.responses);  
+  }
+  
 }
 
